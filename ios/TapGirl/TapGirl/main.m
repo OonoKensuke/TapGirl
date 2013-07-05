fa3c9b2eb61a5313c67ba9ab0a902ab0d672ac7d
nakanomi <nakanomi@gmail.com>
2013-07-05 18:11:33 +0900

    スタックトレース用のコードを追加

--- ios/TapGirl/TapGirl/main.m
+++ ios/TapGirl/TapGirl/main.m
@@ -13,6 +13,16 @@
 int main(int argc, char *argv[])
 {
 	@autoreleasepool {
-	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+		int retVal;
+		@try {
+			retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+		}
+		@catch (NSException *exception) {
+			NSLog(@"%@", [exception callStackSymbols]);
+			@throw exception;
+		}
+		@finally {
+		}
+		return retVal;
 	}
 }
