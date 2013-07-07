3728bc010e1c5ea18ea1f68673d91d7262f3c8fe
nakanomi <nakanomi@gmail.com>
2013-07-07 17:41:26 +0900

    切り替えデータを取得するためのクラスを追加

---
+++ ios/TapGirl/TapGirl/ChangeData.h
@@ -0,0 +1,13 @@
+//
+//  ChangeData.h
+//  TapGirl
+//
+//  Created by nakano_michiharu on 7/7/13.
+//  Copyright (c) 2013 oono. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChangeData : NSObject
+
+@end
---
+++ ios/TapGirl/TapGirl/ChangeData.mm
@@ -0,0 +1,13 @@
+//
+//  ChangeData.mm
+//  TapGirl
+//
+//  Created by nakano_michiharu on 7/7/13.
+//  Copyright (c) 2013 oono. All rights reserved.
+//
+
+#import "ChangeData.h"
+
+@implementation ChangeData
+
+@end
