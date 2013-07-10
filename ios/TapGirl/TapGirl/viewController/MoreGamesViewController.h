6d20054e7d142f4296b60bb3e5bed7c1a13f670d
nakanomi <nakanomi@gmail.com>
2013-07-10 18:33:08 +0900

    その他のゲームを紹介するサイトを表示するビューコントローラーを追加

---
+++ ios/TapGirl/TapGirl/viewController/MoreGamesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoreGamesViewController.h
+//  TapGirl
+//
+//  Created by nakano_michiharu on 7/10/13.
+//  Copyright (c) 2013 oono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreGamesViewController : UIViewController
+
+@end
---
+++ ios/TapGirl/TapGirl/viewController/MoreGamesViewController.m
@@ -0,0 +1,38 @@
+//
+//  MoreGamesViewController.m
+//  TapGirl
+//
+//  Created by nakano_michiharu on 7/10/13.
+//  Copyright (c) 2013 oono. All rights reserved.
+//
+
+#import "MoreGamesViewController.h"
+
+@interface MoreGamesViewController ()
+
+@end
+
+@implementation MoreGamesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
