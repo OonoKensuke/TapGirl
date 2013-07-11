05a67ebc3cd574df3726325141daecc2727b488e
nakanomi <nakanomi@gmail.com>
2013-07-11 18:07:27 +0900

    コメントの追加

--- ios/TapGirl/TapGirl/viewController/MyGLViewController.mm
+++ ios/TapGirl/TapGirl/viewController/MyGLViewController.mm
@@ -13,11 +13,14 @@
 extern "C" {
 #import "GADBannerView.h"
 };
-//Admob用の広告枠コード
 //**********必ずアプリ専用のものに書き換える**********
+//Admob用の広告枠コード
 #define _ADMOB_PUBLISHER_ID  @"a150ff2a3b55ce0"
+//App Bankの広告枠コード、広告ID
 #define _NEND_ID	@"a6eca9dd074372c898dd1df549301f277c53f2b9"
+//App Bankの広告枠コード、SpotID
 #define _NEND_SPOT_ID	@"3172"
+//**********必ずアプリ専用のものに書き換える**********
 
 
 @interface MyGLViewController ()
