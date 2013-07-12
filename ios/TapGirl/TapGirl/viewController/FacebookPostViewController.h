54695f2c77d6532a4025d6d56104036a24ecf8d9
nakanomi <nakanomi@gmail.com>
2013-07-12 15:33:02 +0900

    Facebook投稿用のビューコントローラーを追加

---
+++ ios/TapGirl/TapGirl/viewController/FacebookPostViewController.h
@@ -0,0 +1,13 @@
+//
+//  FacebookPostViewController.h
+//  TapGirl
+//
+//  Created by nakano_michiharu on 7/12/13.
+//  Copyright (c) 2013 oono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FacebookPostViewController : UIViewController
+
+@end
---
+++ ios/TapGirl/TapGirl/viewController/FacebookPostViewController.m
@@ -0,0 +1,38 @@
+//
+//  FacebookPostViewController.m
+//  TapGirl
+//
+//  Created by nakano_michiharu on 7/12/13.
+//  Copyright (c) 2013 oono. All rights reserved.
+//
+
+#import "FacebookPostViewController.h"
+
+@interface FacebookPostViewController ()
+
+@end
+
+@implementation FacebookPostViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
