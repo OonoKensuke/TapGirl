9f1b38923786a4471ee6df227d80a5090efeab5b
nakanomi <nakanomi@gmail.com>
2013-07-12 15:33:30 +0900

    HelloFacebookSampleから必要そうなものを持ってきたところ

--- ios/TapGirl/TapGirl/viewController/FacebookPostViewController.h
+++ ios/TapGirl/TapGirl/viewController/FacebookPostViewController.h
@@ -7,7 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
 
-@interface FacebookPostViewController : UIViewController
+
+@interface FacebookPostViewController : UIViewController<FBLoginViewDelegate>
+{
+	
+}
+@property (retain, nonatomic) IBOutlet UIButton *btnPostMessage;
+@property (retain, nonatomic) IBOutlet UILabel *labelFirstName;
+@property (retain, nonatomic) IBOutlet FBProfilePictureView *profilePic;
+
+- (IBAction)onPushButton:(id)sender;
 
 @end
--- ios/TapGirl/TapGirl/viewController/FacebookPostViewController.m
+++ ios/TapGirl/TapGirl/viewController/FacebookPostViewController.m
@@ -9,10 +9,12 @@
 #import "FacebookPostViewController.h"
 
 @interface FacebookPostViewController ()
+@property (strong, nonatomic) id<FBGraphUser> loggedInUser;
 
 @end
 
 @implementation FacebookPostViewController
+@synthesize loggedInUser = _loggedInUser;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -27,6 +29,14 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    FBLoginView *loginview = [[FBLoginView alloc] init];
+    
+    loginview.frame = CGRectOffset(loginview.frame, 5, 5);
+    loginview.delegate = self;
+    
+    [self.view addSubview:loginview];
+    
+    [loginview sizeToFit];
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,5 +44,179 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)dealloc {
+    [_btnPostMessage release];
+	[_labelFirstName release];
+	[_profilePic release];
+    [super dealloc];
+}
+- (void)viewDidUnload {
+    [self setBtnPostMessage:nil];
+	[self setLabelFirstName:nil];
+	[self setProfilePic:nil];
+    [super viewDidUnload];
+}
+
+
+#pragma mark - FBLoginViewDelegate
+
+- (void)loginViewShowingLoggedInUser:(FBLoginView *)loginView {
+    // first get the buttons set for login mode
+	self.btnPostMessage.enabled = true;
+	
+//    self.buttonPostPhoto.enabled = YES;
+//    self.buttonPostStatus.enabled = YES;
+//    self.buttonPickFriends.enabled = YES;
+//    self.buttonPickPlace.enabled = YES;
+//	
+//    // "Post Status" available when logged on and potentially when logged off.  Differentiate in the label.
+//    [self.buttonPostStatus setTitle:@"Post Status Update (Logged On)" forState:self.buttonPostStatus.state];
+}
+
+- (void)loginViewFetchedUserInfo:(FBLoginView *)loginView
+                            user:(id<FBGraphUser>)user {
+    // here we use helper properties of FBGraphUser to dot-through to first_name and
+    // id properties of the json response from the server; alternatively we could use
+    // NSDictionary methods such as objectForKey to get values from the my json object
+    self.labelFirstName.text = [NSString stringWithFormat:@"Hello %@!", user.first_name];
+    // setting the profileID property of the FBProfilePictureView instance
+    // causes the control to fetch and display the profile picture for the user
+    self.profilePic.profileID = user.id;
+    self.loggedInUser = user;
+}
+
+- (void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView {
+    // test to see if we can use the share dialog built into the Facebook application
+    FBShareDialogParams *p = [[FBShareDialogParams alloc] init];
+    p.link = [NSURL URLWithString:@"http://developers.facebook.com/ios"];
+#ifdef DEBUG
+    [FBSettings enableBetaFeatures:FBBetaFeaturesShareDialog];
+#endif
+//    BOOL canShareFB = [FBDialogs canPresentShareDialogWithParams:p];
+//    BOOL canShareiOS6 = [FBDialogs canPresentOSIntegratedShareDialogWithSession:nil];
+	
+//    self.buttonPostStatus.enabled = canShareFB || canShareiOS6;
+//    self.buttonPostPhoto.enabled = NO;
+//    self.buttonPickFriends.enabled = NO;
+//    self.buttonPickPlace.enabled = NO;
+    
+    // "Post Status" available when logged on and potentially when logged off.  Differentiate in the label.
+//    [self.buttonPostStatus setTitle:@"Post Status Update (Logged Off)" forState:self.buttonPostStatus.state];
+    
+    self.profilePic.profileID = nil;
+    self.labelFirstName.text = nil;
+    self.loggedInUser = nil;
+}
+
+- (void)loginView:(FBLoginView *)loginView handleError:(NSError *)error {
+    // see https://developers.facebook.com/docs/reference/api/errors/ for general guidance on error handling for Facebook API
+    // our policy here is to let the login view handle errors, but to log the results
+    NSLog(@"FBLoginView encountered an error=%@", error);
+}
+#pragma mark -
+
+// Convenience method to perform some action that requires the "publish_actions" permissions.
+- (void) performPublishAction:(void (^)(void)) action {
+    // we defer request for permission to post to the moment of post, then we check for the permission
+    if ([FBSession.activeSession.permissions indexOfObject:@"publish_actions"] == NSNotFound) {
+        // if we don't already have the permission, then we request it now
+        [FBSession.activeSession requestNewPublishPermissions:@[@"publish_actions"]
+                                              defaultAudience:FBSessionDefaultAudienceFriends
+                                            completionHandler:^(FBSession *session, NSError *error) {
+                                                if (!error) {
+                                                    action();
+                                                }
+                                                //For this example, ignore errors (such as if user cancels).
+                                            }];
+    } else {
+        action();
+    }
+    
+}
+
+#pragma mark -event
+- (IBAction)onPushButton:(id)sender
+{
+    NSURL *urlToShare = [NSURL URLWithString:@"http://developers.facebook.com/ios"];
+	if (sender == self.btnPostMessage) {
+		FBAppCall *appCall = [FBDialogs presentShareDialogWithLink:urlToShare
+															  name:@"Hello Facebook"
+														   caption:nil
+													   description:@"The 'Hello Facebook' sample application showcases simple Facebook integration."
+														   picture:nil
+													   clientState:nil
+														   handler:^(FBAppCall *call, NSDictionary *results, NSError *error) {
+															   if (error) {
+																   NSLog(@"Error: %@", error.description);
+															   } else {
+																   NSLog(@"Success!");
+															   }
+														   }];
+		
+		if (!appCall) {
+			// Next try to post using Facebook's iOS6 integration
+			BOOL displayedNativeDialog = [FBDialogs presentOSIntegratedShareDialogModallyFrom:self
+																				  initialText:nil
+																						image:nil
+																						  url:urlToShare
+																					  handler:nil];
+			
+			if (!displayedNativeDialog) {
+				// Lastly, fall back on a request for permissions and a direct post using the Graph API
+				[self performPublishAction:^{
+					NSString *message = [NSString stringWithFormat:@"Updating status for %@ at %@", self.loggedInUser.first_name, [NSDate date]];
+					
+					[FBRequestConnection startForPostStatusUpdate:message
+												completionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+													
+													[self showAlert:message result:result error:error];
+													self.btnPostMessage.enabled = YES;
+												}];
+					
+					self.btnPostMessage.enabled = NO;
+				}];
+			}
+		}
+	}
+}
+
+// UIAlertView helper for post buttons
+- (void)showAlert:(NSString *)message
+           result:(id)result
+            error:(NSError *)error {
+    
+    NSString *alertMsg;
+    NSString *alertTitle;
+    if (error) {
+        alertTitle = @"Error";
+        if (error.fberrorShouldNotifyUser ||
+            error.fberrorCategory == FBErrorCategoryPermissions ||
+            error.fberrorCategory == FBErrorCategoryAuthenticationReopenSession) {
+            alertMsg = error.fberrorUserMessage;
+        } else {
+            alertMsg = @"Operation failed due to a connection problem, retry later.";
+        }
+    } else {
+        NSDictionary *resultDict = (NSDictionary *)result;
+        alertMsg = [NSString stringWithFormat:@"Successfully posted '%@'.", message];
+        NSString *postId = [resultDict valueForKey:@"id"];
+        if (!postId) {
+            postId = [resultDict valueForKey:@"postId"];
+        }
+        if (postId) {
+            alertMsg = [NSString stringWithFormat:@"%@\nPost ID: %@", alertMsg, postId];
+        }
+        alertTitle = @"Success";
+    }
+    
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:alertTitle
+                                                        message:alertMsg
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+    [alertView show];
+}
+
 
 @end
