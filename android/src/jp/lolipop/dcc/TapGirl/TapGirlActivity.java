840242eb0afcf0da7eee75eee1e0348822866af8
nakanomi <nakanomi@gmail.com>
2013-07-24 16:32:32 +0900

    レンダラーがファイルの読み込みなどをするためインスタンスを取得できるようにした

--- android/src/jp/lolipop/dcc/TapGirl/TapGirlActivity.java
+++ android/src/jp/lolipop/dcc/TapGirl/TapGirlActivity.java
@@ -10,10 +10,20 @@ import android.os.Bundle;
 public class TapGirlActivity extends Activity {
 	private GLSurfaceView mGLSurfaceView = null;
 	private MyRenderer mRenderer = null;
+	private static TapGirlActivity s_Instance = null;
+	
+	public static TapGirlActivity getInstance()
+	{
+		return s_Instance;
+	}
+	
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        assert(s_Instance == null);
+        s_Instance = this;
         mRenderer = new MyRenderer();
         mGLSurfaceView = MyGLUtil.initGLES20(this, mRenderer);
         setContentView(mGLSurfaceView);
