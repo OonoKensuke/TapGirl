5a2475404130323814d6be111afa351803784bb5
nakanomi <nakanomi@gmail.com>
2013-07-24 12:49:41 +0900

    rendererクラスのベースを追加

---
+++ android/src/jp/lolipop/dcc/lib/IGLRenderer.java
@@ -0,0 +1,29 @@
+package jp.lolipop.dcc.lib;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+
+public class IGLRenderer implements GLSurfaceView.Renderer {
+
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
