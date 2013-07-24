40cb9cd7680b34fbbe5f7079a56e2380ebe9280f
nakanomi <nakanomi@gmail.com>
2013-07-24 12:51:12 +0900

    開始前のアンインストール用スクリプト追加 アイコン追加

---
+++ android/prevDev.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+/Users/nakano_michiharu/android-sdk-macosx/platform-tools/adb shell setprop debug.assert 1
+/Users/nakano_michiharu/android-sdk-macosx/platform-tools/adb uninstall jp.lolipop.dcc.TapGirl
