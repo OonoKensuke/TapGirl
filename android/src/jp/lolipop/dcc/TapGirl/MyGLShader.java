9ae44de1cd2025b5b6ef41aa2f8261971da1610c
nakanomi <nakanomi@gmail.com>
2013-07-25 15:33:30 +0900

    頂点バッファなどをシェーダーの外に出して描画するテスト

--- android/src/jp/lolipop/dcc/TapGirl/MyGLShader.java
+++ android/src/jp/lolipop/dcc/TapGirl/MyGLShader.java
@@ -86,6 +86,29 @@ public class MyGLShader extends IGLShader {
 	}
 	
 	/**
+	 * @param primitive 表示するプリミティブタイプ。点、トライアングルストリップなど
+	 * @param vertexBuffer 別の場所で確保されている頂点バッファ
+	 * @param textureId　テクスチャID
+	 * @param count　頂点の数
+	 * @param color　描画の際に設定される色
+	 * @param alpha　アルファ
+	 */
+	public void drawArraysMy(int primitive, int vertexBuffer,
+			int textureId, int count, CColor color, float alpha)
+	{
+		// バッファオブジェクトをターゲットにバインドする
+		GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, vertexBuffer);
+		// a_Position変数にバッファオブジェクトを割り当てる
+		GLES20.glVertexAttribPointer(VA_POSITION, 2, GLES20.GL_FLOAT, false, 0, 0);
+
+		// a_Position変数でのバッファオブジェクトの割り当てを有効にする
+		GLES20.glEnableVertexAttribArray(VA_POSITION);
+		
+		GLES20.glDrawArrays(primitive, 0, count);
+	}
+		
+	
+	/**
 	 * @param fileName 読み込むファイルのパス
 	 * @param activity リソースマネージャーを使うのに必要なアクティビティのインスタンス
 	 * @return
--- android/src/jp/lolipop/dcc/TapGirl/MyRenderer.java
+++ android/src/jp/lolipop/dcc/TapGirl/MyRenderer.java
@@ -1,5 +1,7 @@
 package jp.lolipop.dcc.TapGirl;
 
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
@@ -14,12 +16,20 @@ public class MyRenderer extends IGLRenderer {
 	
 	private MyGLShader mShaderCurrent = null;
 	private MyGLShader mShaderNext = null;
+	
+	private FloatBuffer mFloatBufferCurrent = null;
+	private FloatBuffer mFloatBufferNext = null;
+	
+	private int mVertexBufferCurrent = -1;
+	private int mVertexBufferNext = -1;
+	
 	@Override
 	public void onDrawFrame(GL10 gl) {
 		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 		mShaderCurrent.use();
-		mShaderCurrent.testUseMyPosition();
-		GLES20.glDrawArrays(GLES20.GL_POINTS, 0, 1);
+//		mShaderCurrent.testUseMyPosition();
+//		GLES20.glDrawArrays(GLES20.GL_POINTS, 0, 1);
+		mShaderCurrent.drawArraysMy(GLES20.GL_POINTS, mVertexBufferCurrent, 0, 1, null, 1.0f);
 		mShaderNext.use();
 		mShaderNext.testUseMyPosition();
 		GLES20.glDrawArrays(GLES20.GL_POINTS, 0, 1);
@@ -36,13 +46,26 @@ public class MyRenderer extends IGLRenderer {
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		Log.v("info", "MyRenderer#onSurfaceCreated ");
 		GLES20.glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
-		mShaderNext = new MyGLShader();
-		mShaderNext.build(TapGirlActivity.getInstance());
-		mShaderNext.testPoint(0.0f, -0.5f);
+		mFloatBufferCurrent = MyGLUtil.makeFloatBuffer(new float[] { 0.0f, 0.5f });
+		int [] vertexBufVal = new int [1];
+		GLES20.glGenBuffers(1, vertexBufVal, 0);
+		mVertexBufferCurrent = vertexBufVal[0];
+		assert(mVertexBufferCurrent >= 0);
+		
+		//
+		// バッファオブジェクトをターゲットにバインドする
+		GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, mVertexBufferCurrent);
+		// バッファオブジェクトにデータを書き込む
+		GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, MyGLShader.FSIZE * mFloatBufferCurrent.limit(), mFloatBufferCurrent, GLES20.GL_DYNAMIC_DRAW);
 		
 		mShaderCurrent = new MyGLShader();
 		mShaderCurrent.build(TapGirlActivity.getInstance());
-		mShaderCurrent.testPoint(0.0f, 0.5f);
+
+	
+		mShaderNext = new MyGLShader();
+		mShaderNext.build(TapGirlActivity.getInstance());
+		mShaderNext.testPoint(0.0f, -0.5f);
+	
 	}
 
 }
