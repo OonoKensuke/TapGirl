53b7b317a49a41129d2609b5c7b6cf052eea3cc8
nakanomi <nakanomi@gmail.com>
2013-07-28 17:11:14 +0900

    u,v調整

--- android/src/jp/lolipop/dcc/TapGirl/CPrimitive.java
+++ android/src/jp/lolipop/dcc/TapGirl/CPrimitive.java
@@ -34,11 +34,13 @@ public class CPrimitive {
 	void testSetUp(float x, float y)
 	{
 		float size = 1.0f;
+		float u = 640.0f / 1024.0f;
+		float v = (1136.0f - 100.0f) / 2048.0f;
 		mFloatBuffer = MyGLUtil.makeFloatBuffer(new float[] {
 				x - size, y + size,  0.0f, 0.0f,
-				x - size, y - size,  0.0f, 1.0f,
-				x + size, y + size,  1.0f, 0.0f,
-				x + size, y - size,  1.0f, 1.0f,
+				x - size, y - size,  0.0f, v,
+				x + size, y + size,  u,    0.0f,
+				x + size, y - size,  u,    v,
 				});
 		int [] vertexBufVal = new int [1];
 		GLES20.glGenBuffers(1, vertexBufVal, 0);
