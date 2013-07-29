8c8906790c0317ebe7f44b04abf9db6f0d8dc928
nakanomi <nakanomi@gmail.com>
2013-07-29 19:06:07 +0900

    定義関連のクラスを追加

---
+++ android/src/jp/lolipop/dcc/TapGirl/CDefines.java
@@ -0,0 +1,45 @@
+package jp.lolipop.dcc.TapGirl;
+
+public class CDefines {
+// mark -SNS
+
+//SNS拡散URL
+static public final String SNS_URL =	"http://sekai-seifuku.com/";
+//SNSアプリ名
+static public final String SNS_APP_NAME =	"アプリ名";
+//SNSハッシュタグ
+static public final String SNS_HASHTAG =	"ハッシュタグ";
+//Twitterフォーマット
+//#define SNS_TWEET_FORMAT	@"アプリを%dcmこすった。%@ #%@ "
+
+//Facebookキャプション
+static public final String FACEBOOK_CAPTION =	"キャプション";
+//Facebook説明
+static public final String FACEBOOK_DESCRIPTION =	"アプリ説明";
+
+// mark -広告
+
+//**********必ずアプリ専用のものに書き換える**********
+//Admob用の広告枠コード
+static public final String _ADMOB_PUBLISHER_ID =  "a150ff2a3b55ce0";
+//App Bankの広告枠コード、広告ID
+static public final String _NEND_ID =	"a6eca9dd074372c898dd1df549301f277c53f2b9";
+//App Bankの広告枠コード、SpotID
+static public final String _NEND_SPOT_ID =	"3172";
+//**********必ずアプリ専用のものに書き換える**********
+
+// mark -More Apps
+static public final String MORE_APPS_SITE =	"http://sekai-seifuku.com/";
+
+
+// mark -congratulations おめでとう画面
+//最後に数字の場所に表示するメッセージ
+static public final String CONGRATURATIONS_MESSAGE =	"Congratulations!";
+static public final String SHOP_PASSWORD =	"password";
+//パスワードを表示しにいくまでの時間
+static public final float CONGRATULATIONS_WAIT_TO_PASSWORD =	10.0f;
+//ショップサイトを表示しにいくまでの時間
+static public final float  CONGRATULATIONS_WAIT_TO_SHOP =	15.0f;
+//表示するサイト
+static public final String SHOP_SITE_URL =	"http://sekai-seifuku.com/";
+}
